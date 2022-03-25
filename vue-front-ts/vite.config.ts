aed92bfce6186b3dc05c1fe7d0b3f2893a68bef8
zhangsl <zhangsl@polarwin.cn>
2022-03-25 17:32:44 +0800

    简化代码，按初始样式增加样式按需加载组件

--- vue-front-ts/vite.config.ts
+++ vue-front-ts/vite.config.ts
@@ -1,18 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import { wrapperEnv } from './build/utils';
-import { createVitePlugins } from './build/vite/plugin';
-import { loadEnv } from 'vite';
-
-
+// 组件按需引入插件
+import Compoents from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+// 样式按需引入插件
+import { createStyleImportPlugin, AndDesignVueResolve, AntdResolve} from 'vite-plugin-style-import'
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir);
 }
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: createVitePlugins(true),
+  plugins: [
+    vue(),
+    Compoents({
+      resolvers: [
+        AntDesignVueResolver()
+      ]
+    }),
+    createStyleImportPlugin({
+      resolves: [
+        AndDesignVueResolve(),
+        AntdResolve()
+      ],
+      libs: [
+        // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+        {
+          libraryName: 'ant-design-vue',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `ant-design-vue/es/${name}/style/index`
+          },
+        },
+      ],
+    })
+  ],
   resolve: {
     alias: [
       // /@/xxxx => src/xxxx
